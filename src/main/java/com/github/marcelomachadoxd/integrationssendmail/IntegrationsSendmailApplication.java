ab430930bbfa5164ad157c2c7857f8805438ef8f
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-10-13 20:47:44 -0300

    Initial commit

---
+++ src/main/java/com/github/marcelomachadoxd/integrationssendmail/IntegrationsSendmailApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.integrationssendmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationsSendmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationsSendmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marcelomachadoxd/integrationssendmail/IntegrationsSendmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.integrationssendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationsSendmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
