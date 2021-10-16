994ebcc45de50ba329a37666d905725b34081440
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-10-16 16:35:04 -0300

    Sent e-mail (need refactor)

--- src/main/java/com/github/marcelomachadoxd/integrationssendmail/resources/EmailResource.java
+++ src/main/java/com/github/marcelomachadoxd/integrationssendmail/resources/EmailResource.java
@@ -1,6 +1,8 @@
 package com.github.marcelomachadoxd.integrationssendmail.resources;
 
 import com.github.marcelomachadoxd.integrationssendmail.dto.EmailDTO;
+import com.github.marcelomachadoxd.integrationssendmail.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/emails")
 public class EmailResource {
 
+    @Autowired
+    private EmailService emailService;
+
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
-
+        emailService.sendEmail(dto);
         return ResponseEntity.noContent().build();
     }
 
